99ab8704e0ffea3509b6c6cb5b139bb269f094be
notlegendprogrammer <deni.fahri@the-legend.org>
2024-12-28 16:40:34 +0700

    Create penjumlahan_pengurangan.m

---
+++ penjumlahan_pengurangan.m
@@ -0,0 +1,22 @@
+% Definisi Matriks
+A = [1, 2; 3, 4];  % Matriks 2x2
+B = [5, 6; 7, 8];  % Matriks 2x2
+
+% Penjumlahan Matriks
+C = A + B;
+
+% Pengurangan Matriks
+D = B - A;
+
+% Menampilkan Hasil
+disp('Matriks A:');
+disp(A);
+
+disp('Matriks B:');
+disp(B);
+
+disp('Hasil Penjumlahan (A + B):');
+disp(C);
+
+disp('Hasil Pengurangan (B - A):');
+disp(D);
